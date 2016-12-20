4c8ef53715e5ceb37e26b4e63e06e740076b1c42
dimdiden <dimdiden@gmail.com>
2016-12-20 08:39:55 +0200

    added pages with student's list

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.views.generic import ListView, DetailView
 from webapp.models import Group, Student
 from . import views
-from  webapp.views import HomeTemplateView
+from  webapp.views import HomeTemplateView, StudentListView
 
 urlpatterns = [
 	url(r'^$', HomeTemplateView.as_view(), name='home'),
-	url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Group, template_name = 'group.html')),
+#	url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Group, template_name = 'group.html')),
+	url(r'^group/(?P<pk>\d+)$', StudentListView.as_view(), name='group'),
 	url(r'^newgroup/', views.newgroup, name='newgroup'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
-from django.views.generic import  TemplateView, FormView
-from django.core.urlresolvers import reverse_lazy
-from django.contrib.auth import login
+from django.views.generic import  TemplateView, FormView, DetailView
 from webapp.models import Group, Student
 
 class HomeTemplateView(TemplateView):
@@ -15,14 +13,28 @@ class HomeTemplateView(TemplateView):
 	    dic = {}
 		#populate dictionary dic with grups and number of the students
 	    for obj in Group.objects.all():
-
 	    	stdcount = Student.objects.filter(group = obj.id).count()
-
 	    	dic.update({obj : [obj.name, stdcount, obj.head]})
 
 	    context['groups'] = dic
-	    print(context)
 	    return context
+
+
+class StudentListView(DetailView):
+
+	model = Group
+	template_name = "group.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(StudentListView, self).get_context_data(**kwargs)
+
+		self.group = get_object_or_404(Group, id=self.kwargs['pk'])
+
+		context['students'] = Student.objects.filter(group = self.group)
+		context['group'] = Group.objects.get(id = self.kwargs['pk'])
+#		print(context)
+		return context
+
 
 
 
