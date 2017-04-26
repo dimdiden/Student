ee8757a920c7ee4b0230863beed331f071da53b0
dimdiden <dimdiden@gmail.com>
2017-04-26 12:34:00 +0300

    Added description block and slight changes in login/register templates

--- webapp/views.py
+++ webapp/views.py
@@ -22,6 +22,7 @@ class HomeView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['form'] = GroupForm(prefix='group')
+        context['description'] = 'Home page'
         return context
 
 
