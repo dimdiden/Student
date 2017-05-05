0dccc130c9d9f1e8ee1171b79b5d62420d30f597
dimdiden <dimdiden@gmail.com>
2017-05-05 09:34:19 +0300

    Added procfile and modified allowed hosts

--- students/settings.py
+++ students/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9d6mr**^gbux=!75!+12o9^(3^dojgh@=8_vk75-x0u8r_f&d@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
